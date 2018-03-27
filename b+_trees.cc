8c9af85967077eff2de143e7db9a633f92d0fc3b
CharanGotham <charan141212@gmail.com>
2018-03-27 12:03:44 +0530

    test functions

---
+++ b+_trees.cc
@@ -0,0 +1,32 @@
+/*
+ * b+_trees.cc
+ *
+ *  Created on: 27-Mar-2018
+ *      Author: charan_gotham
+ */
+
+#include <iostream>
+
+#include "b+_trees.h"
+
+using namespace std;
+
+void BTree::Insert(int element_to_insert) {
+	return;
+}
+
+void BTree::Find(int elelment_to_find) {
+	return;
+}
+
+int BTree::Count(int element_count) {
+	cout << "passed" << endl;
+	element_count = 0;
+	return element_count;
+}
+
+vector<int> BTree::Range(int start, int end) {
+	vector<int> elements_in_range;
+	elements_in_range.push_back(-1);
+	return elements_in_range;
+}
